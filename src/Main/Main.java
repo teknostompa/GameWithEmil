d68ab33328678dde0cb6b9c9bcc177eaa4ff7a64
teknostompa <evaldsson.tompa@gmail.com>
2019-02-17 00:05:16 +0100

    Suicide

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -33,6 +33,9 @@ public class Main  extends JPanel  implements ActionListener, KeyListener {
     static int h = 600;
 	static int w = 600;
 	int grav;
+	BufferedImage image = null;
+    ArrayList<ArrayList<Integer>> tempLayout = new ArrayList<>();
+	TileLayer layer =null;
 	
 
     public Main() throws IOException {
@@ -45,45 +48,46 @@ public class Main  extends JPanel  implements ActionListener, KeyListener {
 
 
     public void paint(Graphics g) {
-    	BufferedImage image = null;
-		try {
-			image = ImageIO.read(new File("a.jpg"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    	if(image==null) {
+    		try {
+    			image = ImageIO.read(new File("b.jpg"));
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}    		
+    		
+    		try(BufferedReader br = new BufferedReader(new FileReader("World.txt"))){
+    			String currentLine;
+    			while((currentLine = br.readLine()) != null) {
+    				if(currentLine.isEmpty()) {
+    					continue;
+    				}
+    				ArrayList<Integer> row = new ArrayList<>();
+    				
+    				String[] values = currentLine.trim().split(" ");
+    				
+    				for(String string : values) {
+    					if(!string.isEmpty()) {
+    						int id = Integer.parseInt(string);
+    						
+    						row.add(id);
+    					}
+    				}
+    				
+    				tempLayout.add(row);
+    			}
+    		}
+    		catch(IOException e) {
+    			
+    		}
+    		
+    	}
         super.paintComponent(g);
         g.setColor(Color.white);
         g.fillRect(0, 0, w, h);
         g.setColor(Color.blue);
-        g.fillOval(w/2-25, h/2-25, 50, 50);
+        g.fillRect(w/2-25, h/2-25, 50, 50);
         g.setColor(Color.black);
-        ArrayList<ArrayList<Integer>> tempLayout = new ArrayList<>();
 
-		TileLayer layer =null;
-		try(BufferedReader br = new BufferedReader(new FileReader("World.txt"))){
-			String currentLine;
-			while((currentLine = br.readLine()) != null) {
-				if(currentLine.isEmpty()) {
-					continue;
-				}
-				ArrayList<Integer> row = new ArrayList<>();
-				
-				String[] values = currentLine.trim().split(" ");
-				
-				for(String string : values) {
-					if(!string.isEmpty()) {
-						int id = Integer.parseInt(string);
-						
-						row.add(id);
-					}
-				}
-				
-				tempLayout.add(row);
-			}
-		}
-		catch(IOException e) {
-			
-		}
 		
 		int width = tempLayout.get(0).size();
 		int height = tempLayout.size();
@@ -91,23 +95,72 @@ public class Main  extends JPanel  implements ActionListener, KeyListener {
 		for(int a = 0; a<height; a++) {
 			for(int b = 0; b<width;b++) {
 				layer.map[a][b] = tempLayout.get(a).get(b);
-				if(layer.map[a][b] != 0) {
-			        g.drawImage(image, x+(50*b), y+(50*a), 50, 50, null);
+					if(layer.map[a][b] == 1) {
+				        g.drawImage(image, x+(50*b), y+(50*a), 50, 50, null);
+				    }
 				}
 			}
-			
-		}
-    }
-
-
-    public void actionPerformed(ActionEvent e) {  
-        velX=posx-negx;
+		int xpos=(x-h/2-25)/-50;
+		int ypos=(y-w/2-25)/-50;
+		velX=posx-negx;
         velY=posy-negy;
-        x = x + velX;
-        y = y + velY;
+        int movex = 1;
+        int movey = 1;
+        for(int yp=-24; yp<25;yp++) {
+        	xpos=(x-h/2-25)/-50;
+			ypos=(y-w/2+yp)/-50;
+            if(layer.map[ypos][xpos]==0) {
+            	
+    	    }else {
+    	    	if(velX<0) {
+    	    		movex=0;
+    	    	}
+    	    }
+    		xpos=(x-h/2+25)/-50;
+    		ypos=(y-w/2+yp)/-50;
+            if(layer.map[ypos][xpos]==0) {
+            	
+    	    }else {
+    	    	if(velX>0) {
+    	    		movex=0;
+    	    	}
+    	    }
+        }
+        if(movex == 1) {
+	        x = x + velX;
+        }
+        for(int xp=-24; xp<25;xp++) {
+        	xpos=(x-h/2+xp)/-50;
+			ypos=(y-w/2-25)/-50;
+            if(layer.map[ypos][xpos]==0) {
+            	
+    	    }else {
+    	    	if(velY<0) {
+    	    		movey=0;
+    	    	}
+    	    }
+    		xpos=(x-h/2+xp)/-50;
+    		ypos=(y-w/2+25)/-50;
+            if(layer.map[ypos][xpos]==0) {
+            	
+    	    }else {
+    	    	if(velY>0) {
+    	    		movey=0;
+    	    	}
+    	    }
+        }
+        if(movey==1) {
+        	y = y + velY;
+        }
+	    System.out.println(layer.map[xpos][ypos]);
         setFocusable(true);
         requestFocusInWindow();
         repaint();
+		}
+
+
+    public void actionPerformed(ActionEvent e) {
+        
     }
     public void keyPressed(KeyEvent e) {
         int c = e.getKeyCode();
